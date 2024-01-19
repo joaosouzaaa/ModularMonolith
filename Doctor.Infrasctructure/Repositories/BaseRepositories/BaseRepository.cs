abf7bd02d71ab8f85387b2d5bb60fee4db15e861
joaosouzaaa <joaoasouza982@gmail.com>
2024-01-18 22:06:10 -0300

    Feat: Configuring base repository for doctor

---
+++ Doctor.Infrasctructure/Repositories/BaseRepositories/BaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using Doctor.Infrasctructure.DatabaseContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Doctor.Infrasctructure.Repositories.BaseRepositories;
+public abstract class BaseRepository<TEntity>(DoctorDbContext dbContext) : IDisposable
+    where TEntity : class
+{
+    private readonly DoctorDbContext _dbContext = dbContext;
+    protected DbSet<TEntity> DbContextSet => _dbContext.Set<TEntity>();
+
+    public void Dispose()
+    {
+        _dbContext.Dispose();
+
+        GC.SuppressFinalize(this);
+    }
+
+    protected async Task<bool> SaveChangesAsync() =>
+        await _dbContext.SaveChangesAsync() > 0;
+}
